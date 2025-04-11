4e84f3c5829c031debc9fb0540ff7824746128e2
Thiago Lima <150944198+ThiagoLimaC@users.noreply.github.com>
2025-04-11 12:35:54 -0300

    Refactor LINQ processing in Program.cs
    Refactored the LINQ processing of `listaNumeros` in `Program.cs`:
    - Commented out the original `Skip` and `Dump` line.
    - Added `SkipWhile` with `Dump` to skip elements while `x < 5`.
    - Added `TakeWhile` with `Dump` to take elements while `x < 2`.
    - Included comments explaining `Skip`, `Take`, `SkipLast`, and `TakeLast` methods, though these lines are commented out.

--- Program.cs
+++ Program.cs
@@ -6,4 +6,18 @@ IEnumerable<int> listaNumeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 // WHERE
 // listaNumeros.Where(num => num > 5).Dump();
 
-listaNumeros.Skip(5).Dump();
+// SKIP e TAKE
+// pula os 5 primeiros
+//listaNumeros.Skip(5).Dump();
+// pega os 5 primeiros
+//listaNumeros.Take(5).Dump();
+
+// SkipLast e TakeLast
+// pula os 5 últimos
+//listaNumeros.SkipLast(5).Dump();
+// pega os 5 últimos
+//listaNumeros.TakeLast(5).Dump();
+
+// SkipWhile e TakeWhile
+listaNumeros.SkipWhile(x => x < 5).Dump();
+listaNumeros.TakeWhile(x => x < 2).Dump();
