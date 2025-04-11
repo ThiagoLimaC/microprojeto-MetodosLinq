dc6601c6df115d4993781603a696384f5d09784b
Thiago Lima <150944198+ThiagoLimaC@users.noreply.github.com>
2025-04-11 12:27:37 -0300

    Change filtering logic in Program.cs to use Skip method
    Updated the filtering logic in Program.cs to use the Skip method instead of the Where method. The new code skips the first 5 elements of the listaNumeros collection and then calls Dump() on the remaining elements, altering the subset of numbers processed and displayed.

--- Program.cs
+++ Program.cs
@@ -4,5 +4,6 @@ using Dumpify;
 IEnumerable<int> listaNumeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 // WHERE
+// listaNumeros.Where(num => num > 5).Dump();
 
-listaNumeros.Where(num => num > 2).Dump();
+listaNumeros.Skip(5).Dump();
